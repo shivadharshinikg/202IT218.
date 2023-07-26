140ff71c422063fe8e0d35a4fd0859e3e159c0a6
shivadharshinikg <106231236+shivadharshinikg@users.noreply.github.com>
2023-07-26 11:16:21 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,29 +1,78 @@
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
-    function App() {
-      const NumberList = ({ numbers }) => {
-        return (
-          <div classnmae="App">
-          <ul>
-          {numbers.map((number, index) => (
-            <li key={index}>{number}</li>
-          ))}
-        </ul>
-        </div>
-      );
 
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userInput: "",
+            list: [],
+        };
     }
 
-};
 
-const App = () => {
-  const numbers = data.numbers;
-  return (
-    <div>
-      <h1>Output</h1>
-      <NumberList numbers={numbers} />
-    </div>
-  );
-};
+    updateInput(value) {
+        this.setState({
+            userInput: value,
+        });
+    }
+
+
+    addItem() {
+        if (this.state.userInput !== "") {
+            const userInput = {
+
+                id: Math.random(),
+                value: this.state.userInput,
+            };
+            const list = [...this.state.list];
+            list.push(userInput);
+            this.setState({
+                list,
+                userInput: "",
+            });
+        }
+    }
+
+    deleteItem(key) {
+        const list = [...this.state.list];
+        const updateList = list.filter((item) => item.id !== key);
+        this.setState({
+            list: updateList,
+        });
+    }
+
+    editItem = (index) => {
+      const todos = [...this.state.list];
+      const editedTodo = prompt('Edit the todo:');
+      if (editedTodo !== null && editedTodo.trim() !== '') {
+        let updatedTodos = [...todos]
+        updatedTodos[index].value= editedTodo
+        this.setState({
+          list: updatedTodos,
+      });
+      }
+    }
+
+    render() {
+        return (
+            <Container>
+                <Row
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        fontSize: "3rem",
+                        fontWeight: "bolder",
+                    }}
+                >
+                    TRAIN SCHEDULE
+                </Row>
+
+
 
 export default App;
